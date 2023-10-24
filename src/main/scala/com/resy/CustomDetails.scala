209d3773620a62e69baa44497c9789b6f6fd9fbd
Brandon Fiegoli <bfiegoli@gmail.com>
2023-10-24 09:09:52 -0400

    Updated Restaurant Details

--- src/main/scala/com/resy/CustomDetails.scala
+++ src/main/scala/com/resy/CustomDetails.scala
@@ -10,8 +10,7 @@ object  CustomDetails {
 
   val api_key: String = "VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5"
   // Called ResyAPI api_key="VbWk7s3L4KiK5fzlO7JD3Q5EYolJI7n5" in the code inspector
-  //Find this info in find?lat in headers - Changes ~ once per month
-
+  //Find this info in find?lat in headers - Changes ~ once per month and is right below the auth_token
 
   //TODO REMEMBER TO CHANGE ALL BELOW
 
@@ -19,21 +18,21 @@ object  CustomDetails {
   val venueId = "65452"
 
   // YYYY-MM-DD of reservation
-  val pref_day = "2023-11-19"
+  val pref_day = "2023-11-20"
 
   //indoor or outdoor etc. Should match the type exactly on the resy venue (case doesn't matter). Leave blank if you don't care or don't now
   val dining_type= ""
   //If you don't want a dining type just use ""
 
-  // Seq of YYYY-MM-DD HH:MM:SS times of reservations in military time format in EST
+  // Seq of YYYY-MM-DD HH:MM:SS times of reservations in military time format in EST. This list will run in order of preference
   val pref_times = Seq(
 
-    "2023-11-19 19:30:00",
-    "2023-11-19 19:00:00",
-    "2023-11-19 20:00:00",
-    "2023-11-19 20:30:00",
-    "2023-11-19 18:30:00",
-    "2023-11-19 18:00:00"
+    "2023-11-20 19:30:00",
+    "2023-11-20 19:00:00",
+    "2023-11-20 20:00:00",
+    "2023-11-20 20:30:00",
+    "2023-11-20 18:30:00",
+    "2023-11-20 18:00:00"
 
   )
 
@@ -61,13 +60,13 @@ object  CustomDetails {
 //Laser Wolf = 58848 - 10 am - 21 days out - "Dining Room" - Tables up to 8
 //The Nine's = 7490 - 14 days in advance - "Dinner"
 //Wenwen = 59536 - 12 am - 14 days in advance - "Dining"
-//Rezdora = 5771 - 30 days in advance - "Dining Room"
+//Rezdora = 5771 - 12 am - 30 days in advance - "Dining Room"
 //Peak = 10087 - 9 am - 21 days in advance - "Dining Room"
 //iSodi = 443 - 12 am - 13 days in Advance - "Dining Room", "Sidewalk"
 //Ernestos = 6588 - 12 am - 14 days in advance - "Dining Room"
-//Lillia = 418 - 30 days in advance - "Indoor", "Piazza"
+//Lillia = 418 - 10 am - 30 days in advance - "Indoor", "Piazza"
 //Frenchette = 1946 - 30 days in adcance - "Dining Room"
-//Misi = 2015 = 28 days in advance - "Dining Room"
+//Misi = 2015 = 10 am - 28 days in advance - "Dining Room"
 //Bangkok Supper Club = 73418 - "Main Dining Room"
 //Semma = 1263 - 12 am - 30 days in advance - "Dining Room"
 //Dhamaka = 48994 - 12 am - 30 days in advance - "Indoor"
@@ -79,7 +78,7 @@ object  CustomDetails {
 //AMEX Centurion Lounge = 66930 - 9 am - 30 days in advance
 //Blue Box Cafe = 70161 = 12 am - 30 days in advance - "Dining Room"
 
-//To find new ID, inspect and go to network tab, find?lat and take response into JSON viewer
+//To find new ID, inspect and go to network tab, find?lat and take request response into JSON viewer
 
 
 //"2022-06-18 19:30:00",
